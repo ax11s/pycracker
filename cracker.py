7cac976cf1889984eabfa4f598f30739378011ed
ax11 <125132362+ax11s@users.noreply.github.com>
2023-04-02 00:34:43 +0200

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,11 +1,37 @@
 import itertools
+import threading
 
 password = input("Enter the password to crack: ")
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+matches = []
 
-for guess in itertools.product(characters, repeat=5):
-    attempt = "".join(guess)
-    print(f"Trying: {attempt}")
-    if attempt == password:
-        print(f"Password cracked: {attempt}")
-        break
+def brute(StartingPoint, End):
+    for length in range(StartingPoint, End + 1):
+        for guess in itertools.product(characters, repeat=length):
+            attempt = "".join(guess)
+            print(attempt)
+            if attempt == password:
+                matches.append(attempt)
+                return
+
+
+midpoint = len(password) // 2 
+midpoint = midpoint + 1
+print(midpoint)
+
+t1 = threading.Thread(target=brute, args=(1, midpoint))
+t1.start()
+t1.join()
+
+t2 = threading.Thread(target=brute, args=(midpoint + 2, len(password)))
+t2.start()
+t2.join()
+
+
+
+
+
+if matches:
+    print(f"Password cracked: {matches[0]}")
+else:
+    print("Password not found")
